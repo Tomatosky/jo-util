e3262f5348442cb65888811be74300d9ec5c1e3e
lzk <12345678@qq.com>
2025-02-21 18:14:54 +0800

    update

--- randomUtil/randomUtil.go
+++ randomUtil/randomUtil.go
@@ -2,24 +2,17 @@ package randomUtil
 
 import (
 	"math/rand"
+	"time"
 )
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 // RandomInt 生成指定范围 [start, end) 的随机整数
 func RandomInt(start, end int) int {
 	if start >= end {
 		panic("invalid range: start >= end")
 	}
-	return rand.Intn(end-start) + start
-}
-
-// RandomBytes 生成指定长度的随机字节切片
-func RandomBytes(length int) []byte {
-	if length < 0 {
-		panic("length cannot be negative")
-	}
-	b := make([]byte, length)
-	_, _ = rand.Read(b) // 忽略错误，因为 math/rand 的 Read 不会返回错误
-	return b
+	return rng.Intn(end-start) + start
 }
 
 // RandomEle 从切片中随机选择一个元素
@@ -27,7 +20,7 @@ func RandomEle[T any](slice []T) T {
 	if len(slice) == 0 {
 		panic("slice is empty")
 	}
-	return slice[rand.Intn(len(slice))]
+	return slice[rng.Intn(len(slice))]
 }
 
 // RandomEleSet 从切片中随机选择 n 个不重复的元素
@@ -42,7 +35,7 @@ func RandomEleSet[T any](slice []T, n int) []T {
 	if n > length {
 		n = length
 	}
-	indices := rand.Perm(length)
+	indices := rng.Perm(length)
 	result := make([]T, n)
 	for i := 0; i < n; i++ {
 		result[i] = slice[indices[i]]
@@ -50,12 +43,41 @@ func RandomEleSet[T any](slice []T, n int) []T {
 	return result
 }
 
+// RandomWeightedKey 根据权重随机选择一个键
+func RandomWeightedKey[T comparable](weights map[T]int) T {
+	// 计算总权重
+	var sum int
+	for _, w := range weights {
+		sum += w
+	}
+
+	// 处理无效权重的情况
+	if sum == 0 {
+		panic("所有权重值总和不能为0")
+	}
+
+	// 生成随机数
+	r := rng.Intn(sum)
+
+	// 查找对应的键
+	var runningTotal int
+	for key, weight := range weights {
+		runningTotal += weight
+		if runningTotal > r {
+			return key
+		}
+	}
+
+	// 理论上不会执行到这里（因为sum > 0）
+	panic("未找到有效键")
+}
+
 // RandomString 生成包含数字和字母的随机字符串
 func RandomString(length int) string {
 	const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+		b[i] = charset[rng.Intn(len(charset))]
 	}
 	return string(b)
 }
@@ -65,7 +87,7 @@ func RandomNumbers(length int) string {
 	const charset = "0123456789"
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+		b[i] = charset[rng.Intn(len(charset))]
 	}
 	return string(b)
 }
