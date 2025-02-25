b03ddcc53e9be97a36d7413755198a3905550fd4
lzk <12345678@qq.com>
2025-02-25 22:13:50 +0800

    update

--- strUtil/strUtil.go
+++ strUtil/strUtil.go
@@ -1,6 +1,7 @@
 package strUtil
 
 import (
+	"encoding/json"
 	"strconv"
 )
 
@@ -47,4 +48,47 @@ func ToFloat64(s string) float64 {
 		panic(err)
 	}
 	return f64
+}
+
+func ToString(value any) string {
+	if value == nil {
+		return ""
+	}
+
+	switch val := value.(type) {
+	case float32:
+		return strconv.FormatFloat(float64(val), 'f', -1, 32)
+	case float64:
+		return strconv.FormatFloat(val, 'f', -1, 64)
+	case int:
+		return strconv.FormatInt(int64(val), 10)
+	case int8:
+		return strconv.FormatInt(int64(val), 10)
+	case int16:
+		return strconv.FormatInt(int64(val), 10)
+	case int32:
+		return strconv.FormatInt(int64(val), 10)
+	case int64:
+		return strconv.FormatInt(val, 10)
+	case uint:
+		return strconv.FormatUint(uint64(val), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(val), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(val), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(val), 10)
+	case uint64:
+		return strconv.FormatUint(val, 10)
+	case string:
+		return val
+	case []byte:
+		return string(val)
+	default:
+		b, err := json.Marshal(val)
+		if err != nil {
+			return ""
+		}
+		return string(b)
+	}
 }
