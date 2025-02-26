ec51de2c340583018a47148cb207aa002bde397f
lzk <12345678@qq.com>
2025-02-26 09:14:05 +0800

    update

--- dateUtil/dateUtil.go
+++ dateUtil/dateUtil.go
@@ -11,6 +11,7 @@ var loc *time.Location
 var timeFormat map[string]string
 
 func init() {
+	loc, _ = time.LoadLocation("Asia/Shanghai")
 	timeFormat = map[string]string{
 		"yyyy-mm-dd hh:mm:ss": "2006-01-02 15:04:05",
 		"yyyy-mm-dd hh:mm":    "2006-01-02 15:04",
@@ -37,20 +38,13 @@ func init() {
 	}
 }
 
-func getTime(timestamp int64) time.Time {
-	if loc == nil {
-		var err error
-		loc, err = time.LoadLocation("Asia/Shanghai")
-		if err != nil {
-			panic(err) // 初始化时区失败，程序无法继续
-		}
-	}
+func GetTime(timestamp int64) time.Time {
 	return time.Unix(timestamp, 0).In(loc)
 }
 
 // BeginOfDay 获取某天的起始时间戳(秒时间戳)
 func BeginOfDay(timestamp int64) int64 {
-	t := getTime(timestamp)
+	t := GetTime(timestamp)
 	year, month, day := t.Date()
 	begin := time.Date(year, month, day, 0, 0, 0, 0, loc)
 	return begin.Unix()
@@ -58,7 +52,7 @@ func BeginOfDay(timestamp int64) int64 {
 
 // BeginOfWeek 获取本周一零点时间戳(秒时间戳)
 func BeginOfWeek(timestamp int64) int64 {
-	t := getTime(timestamp)
+	t := GetTime(timestamp)
 
 	// 计算与周一的日期差
 	weekday := t.Weekday()
@@ -75,7 +69,7 @@ func BeginOfWeek(timestamp int64) int64 {
 
 // BeginOfMonth 获取当月第一天零点时间戳(秒时间戳)
 func BeginOfMonth(timestamp int64) int64 {
-	t := getTime(timestamp)
+	t := GetTime(timestamp)
 	year, month, _ := t.Date()
 	return time.Date(year, month, 1, 0, 0, 0, 0, loc).Unix()
 }
@@ -106,7 +100,7 @@ func EndOfWeek(timestamp int64) int64 {
 
 // EndOfMonth 获取当月最后一天23:59:59时间戳(秒时间戳)
 func EndOfMonth(timestamp int64) int64 {
-	t := getTime(timestamp)
+	t := GetTime(timestamp)
 	// 计算下个月第一天的零点时间戳，减1秒即为当月最后时间
 	nextMonthFirstDay := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, loc)
 	return nextMonthFirstDay.Unix() - 1
@@ -114,13 +108,13 @@ func EndOfMonth(timestamp int64) int64 {
 
 // Format 格式化时间戳
 func Format(timestamp int64) string {
-	return getTime(timestamp).Format("2006-01-02 15:04:05")
+	return GetTime(timestamp).Format("2006-01-02 15:04:05")
 }
 
 // IsSameDay 判断两个时间戳是否在同一天
 func IsSameDay(t1, t2 int64) bool {
-	time1 := getTime(t1)
-	time2 := getTime(t2)
+	time1 := GetTime(t1)
+	time2 := GetTime(t2)
 
 	y1, m1, d1 := time1.Date()
 	y2, m2, d2 := time2.Date()
