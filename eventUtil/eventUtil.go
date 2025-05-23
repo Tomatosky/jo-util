f39cb86e934e43de12f97c8737a3bd8cb9e04f5e
admin <123@qq.com>
2025-05-23 20:41:41 +0800

    update

--- eventUtil/eventUtil.go
+++ eventUtil/eventUtil.go
@@ -2,6 +2,7 @@ package eventUtil
 
 import (
 	"errors"
+	"github.com/panjf2000/ants/v2"
 	"sync"
 )
 
@@ -11,16 +12,22 @@ type EventHandler func(data interface{})
 // EventManager 事件管理器结构
 type EventManager struct {
 	handlers   map[string][]EventHandler
+	pool       *ants.Pool
 	lock       sync.RWMutex
 	destroying bool
 	wg         sync.WaitGroup // 用于等待所有事件处理完成
 }
 
 // NewEventManager 创建一个新的事件管理器
-func NewEventManager() *EventManager {
+func NewEventManager() (*EventManager, error) {
+	pool, err := ants.NewPool(ants.DefaultAntsPoolSize)
+	if err != nil {
+		return nil, err
+	}
 	return &EventManager{
 		handlers: make(map[string][]EventHandler),
-	}
+		pool:     pool,
+	}, nil
 }
 
 // Register 注册事件处理函数
@@ -43,61 +50,30 @@ func (em *EventManager) Register(eventName string, handler EventHandler) error {
 	return nil
 }
 
-// Unregister 取消注册事件处理函数
-func (em *EventManager) Unregister(eventName string, handler EventHandler) error {
-	if eventName == "" {
-		return errors.New("event name cannot be empty")
-	}
-	if handler == nil {
-		return errors.New("handler cannot be nil")
-	}
-
-	em.lock.Lock()
-	defer em.lock.Unlock()
-
-	if em.destroying {
-		return errors.New("event manager is destroying, cannot unregister handlers")
-	}
-
-	handlers, exists := em.handlers[eventName]
-	if !exists {
-		return errors.New("event not found")
-	}
-
-	for i, h := range handlers {
-		if &h == &handler { // 比较函数指针
-			em.handlers[eventName] = append(handlers[:i], handlers[i+1:]...)
-			return nil
-		}
-	}
-
-	return errors.New("handler not found for the event")
-}
-
 // Trigger 触发事件
 func (em *EventManager) Trigger(eventName string, data interface{}) error {
 	if em.isDestroying() {
 		return errors.New("event manager is destroying, cannot trigger events")
 	}
 
+	if eventName == "" {
+		return errors.New("event name cannot be empty")
+	}
+
 	em.wg.Add(1)
-	go func() {
+	err := em.pool.Submit(func() {
 		defer em.wg.Done()
 		em.triggerSync(eventName, data)
-	}()
+	})
+	if err != nil {
+		em.wg.Done()
+		return err
+	}
 	return nil
 }
 
 // triggerSync 同步触发事件
 func (em *EventManager) triggerSync(eventName string, data interface{}) error {
-	if eventName == "" {
-		return errors.New("event name cannot be empty")
-	}
-
-	if em.isDestroying() {
-		return errors.New("event manager is destroying, cannot trigger events")
-	}
-
 	em.lock.RLock()
 	handlers, exists := em.handlers[eventName]
 	em.lock.RUnlock()
@@ -133,14 +109,14 @@ func (em *EventManager) Clear() {
 
 // OnDestroy 销毁事件管理器
 func (em *EventManager) OnDestroy() {
+	defer em.pool.Release()
+
 	em.lock.Lock()
 	// 设置销毁标志，阻止新的事件注册和触发
 	em.destroying = true
 	em.lock.Unlock()
-
 	// 等待所有正在处理的事件完成
 	em.wg.Wait()
-
 	// 清除所有事件处理函数
 	em.Clear()
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,11 @@ require (
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/fatih/structs v1.1.0
 	github.com/google/uuid v1.6.0
+	github.com/panjf2000/ants/v2 v2.11.3
 	go.uber.org/zap v1.27.0
 )
 
-require go.uber.org/multierr v1.10.0 // indirect
+require (
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+)
