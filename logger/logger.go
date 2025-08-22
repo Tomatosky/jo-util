7432ad4f0df170af3bb2d690156807d4718c3678
lzk <123@qq.com>
2025-08-22 10:28:21 +0800

    update

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 	"io"
 	"os"
 )
@@ -39,4 +40,15 @@ func InitLog(w io.Writer) *zap.Logger {
 	}
 
 	return zap.New(zapcore.NewTee(coreArr...), zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+}
+
+func SimplyInit(filename string) *zap.Logger {
+	return InitLog(&lumberjack.Logger{
+		Filename:   filename, //日志文件存放目录，如果文件夹不存在会自动创建
+		MaxSize:    50,       //文件大小限制,单位MB
+		MaxBackups: 10,       //最大保留日志文件数量
+		MaxAge:     30,       //日志文件保留天数
+		Compress:   false,    //是否压缩处理
+		LocalTime:  true,
+	})
 }
