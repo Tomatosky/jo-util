4538dc3018cb584996f1749bb29b82d6aa6bd46a
lzk <123@qq.com>
2026-01-17 10:01:41 +0800

    update

--- logger/logger.go
+++ logger/logger.go
@@ -15,15 +15,51 @@ func init() {
 	Log = InitLog(nil)
 }
 
-func InitLog(w io.Writer) *zap.Logger {
+func SimplyInit(logPath string) *zap.Logger {
+	info, err := os.Stat(logPath)
+	if err != nil && !os.IsNotExist(err) {
+		panic(err)
+	}
+	if err == nil && !info.IsDir() {
+		panic("logPath exists but is not a directory: " + logPath)
+	}
+	if os.IsNotExist(err) {
+		_ = os.MkdirAll(logPath, 0755)
+	}
+	infoPath := logPath + "/info.log"
+	warnPath := logPath + "/warn.log"
+	errorPath := logPath + "/error.log"
+	return InitLog(map[io.Writer]zapcore.Level{
+		&lumberjack.Logger{
+			Filename:   infoPath, //日志文件存放目录，如果文件夹不存在会自动创建
+			MaxSize:    50,       //文件大小限制,单位MB
+			MaxBackups: 10,       //最大保留日志文件数量
+			MaxAge:     30,       //日志文件保留天数
+			LocalTime:  true,
+		}: zapcore.InfoLevel,
+		&lumberjack.Logger{
+			Filename:   warnPath, //日志文件存放目录，如果文件夹不存在会自动创建
+			MaxSize:    50,       //文件大小限制,单位MB
+			MaxBackups: 10,       //最大保留日志文件数量
+			MaxAge:     60,       //日志文件保留天数
+			LocalTime:  true,
+		}: zapcore.WarnLevel,
+		&lumberjack.Logger{
+			Filename:   errorPath, //日志文件存放目录，如果文件夹不存在会自动创建
+			MaxSize:    50,        //文件大小限制,单位MB
+			MaxBackups: 10,        //最大保留日志文件数量
+			MaxAge:     90,        //日志文件保留天数
+			LocalTime:  true,
+		}: zapcore.ErrorLevel,
+	})
+}
+
+func InitLog(writers map[io.Writer]zapcore.Level) *zap.Logger {
 	var coreArr []zapcore.Core
 
 	//日志级别
 	debugLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
 		return lev <= zap.FatalLevel && lev >= zap.DebugLevel
-	})
-	infoLevel := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
-		return lev <= zap.FatalLevel && lev >= zap.InfoLevel
 	})
 
 	consoleEncoderConfig := zap.NewProductionEncoderConfig()
@@ -34,26 +70,22 @@ func InitLog(w io.Writer) *zap.Logger {
 	consoleCore := zapcore.NewCore(colorEncoder, zapcore.AddSync(os.Stdout), debugLevel)
 	coreArr = append(coreArr, consoleCore)
 
-	if w != nil {
+	if writers != nil && len(writers) > 0 {
+		// 添加多个 writer
 		fileEncoderConfig := zap.NewProductionEncoderConfig()
 		fileEncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000")
 		fileEncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 		fileEncoder := zapcore.NewJSONEncoder(fileEncoderConfig)
-		fileWriteSyncer := zapcore.AddSync(w)
-		fileCore := zapcore.NewCore(fileEncoder, fileWriteSyncer, infoLevel)
-		coreArr = append(coreArr, fileCore)
+
+		for w, level := range writers {
+			levelEnabler := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+				return lev <= zap.FatalLevel && lev >= level
+			})
+			fileWriteSyncer := zapcore.AddSync(w)
+			fileCore := zapcore.NewCore(fileEncoder, fileWriteSyncer, levelEnabler)
+			coreArr = append(coreArr, fileCore)
+		}
 	}
 
 	return zap.New(zapcore.NewTee(coreArr...), zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-}
-
-func SimplyInit(filename string) *zap.Logger {
-	return InitLog(&lumberjack.Logger{
-		Filename:   filename, //日志文件存放目录，如果文件夹不存在会自动创建
-		MaxSize:    50,       //文件大小限制,单位MB
-		MaxBackups: 10,       //最大保留日志文件数量
-		MaxAge:     30,       //日志文件保留天数
-		Compress:   false,    //是否压缩处理
-		LocalTime:  true,
-	})
 }
